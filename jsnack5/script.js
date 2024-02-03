035032705ae35d093c6d0061cde5fe09a9b9205d
marco <lazzari.marco.ml@gmail.com>
2024-02-03 13:57:54 +0100

    js5 setup base script fatto

---
+++ jsnack5/script.js
@@ -0,0 +1,17 @@
+/* Snack 5:
+Stampa il cubo dei primi N numeri,
+(partendo da 1, il numero N indicato dall'utente è compreso)
+dove N è un numero indicato dall’utente. */
+
+const inputElement = document.querySelector("#user-number");
+const buttonElement = document.querySelector("#button");
+const resultElement = document.querySelector("#result");
+
+buttonElement.addEventListener('click',
+    function(){
+        userValue = inputElement.value;
+        
+        
+        
+    }
+)
