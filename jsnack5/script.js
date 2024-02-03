5e87a0ad2a5fdbdc32581e70eecbf07b294776ff
marco <lazzari.marco.ml@gmail.com>
2024-02-03 17:05:18 +0100

    jsnack5 completato

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -10,12 +10,15 @@ const resultElement = document.querySelector("#result");
 buttonElement.addEventListener('click',
     function(){
         const userInput = inputElement.value;
+        // set up empty array for our cubes
         const cubes = [];
 
+        // counts from 1 to user number
         for(i = 1; i <= userInput; i++){
+            // cubes current number and adds to the cubes array
             cubes.push(Math.pow(i, 3));
         }
-        
+        // print
         resultElement.innerHTML = `Here is the cube of each number between 1 and ${userInput} <br> ${cubes}`
     }
 )
